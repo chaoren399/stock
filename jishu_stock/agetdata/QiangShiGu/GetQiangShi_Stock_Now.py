7a98db5d44a3a87ca0f0370d4e13829c9418e579
mac <715740383@qq.com>
2022-02-06 08:57:32 +0800

    2022年02月06日

--- jishu_stock/agetdata/QiangShiGu/GetQiangShi_Stock_Now.py
+++ jishu_stock/agetdata/QiangShiGu/GetQiangShi_Stock_Now.py
@@ -19,6 +19,8 @@ sys.setdefaultencoding('utf8')
 
 强者恒强, 思路:
 
+20210121 - 20210128 大盘下跌,  28日收盘价 大于 21 日收盘价
+
 2021年12月13日 -2021年12月20日大盘走势是 下跌趋势.
 我们找到 个股  在相同日期 中成上涨趋势的股票 ,这样就有了一个强于大盘的 股票池
 
@@ -27,7 +29,8 @@ sys.setdefaultencoding('utf8')
 '''
 
 def getallQiang_stock_12():
-    path = BASE_DIR + '/jishu_stock/stockdata/stockcodelist_No_ST-1.csv'
+    # path = BASE_DIR + '/jishu_stock/stockdata/stockcodelist_No_ST-1.csv'
+    path = BASE_DIR + '/jishu_stock/stockdata/stockcodelist_No_ST.csv'
     count = 0
     data = pd.read_csv(path, dtype={'code': str})
 
@@ -35,66 +38,35 @@ def getallQiang_stock_12():
     for index, row in data.iterrows():
         # print row['ts_code']
         stock_code = row['ts_code']
-        start_date='20211213'
-        end_date = '20211220'
+        start_date='20220121'
+        end_date = '20220128'
         df = ts.pro_bar(ts_code=stock_code, start_date=start_date, end_date=end_date)
+
+        df = df.sort_values(by='trade_date', axis=0, ascending=True)  # 按照日期 从旧到新 排序
+        df = df.reset_index(drop=True)  # 重新建立索引 ,默认为false，索引列（被设置为索引的列）被还原为普通列，并将索引重置为整数索引，否则直接丢弃索引列。
+
+
+
         # print df
         len_df =len(df)
-        # if(len(df) ==8 ):
+        if(len(df) >0 ):
 
-        time.sleep(0.1)
-
-        # print count
-        close_end = df.ix[0]['close']
-        close_start = df.ix[len_df-1]['close']
-
-        if(close_end > close_start):
-            count = count + 1
-            # line = {'ts_code': stock_code}
-            # path1 = BASE_DIR + '/jishu_stock/stockdata/qiangshi_stock.csv'
-            path1 = 'qiangshi_stock.csv'
-            writeLog_to_txt_path(stock_code,path1)
-            print stock_code
-    print count
-
-
-'''
-
-
-强者恒强, 思路:
-
-2021年08月11日 -2021年08月20日大盘走势是 下跌趋势.
-我们找到 个股  在相同日期 中成上涨趋势的股票 ,这样就有了一个强于大盘的 股票池
-
-20日 的收盘价 大于 11 日的收盘价 就可以
-'''
-
-def getallQiang_stock():
-    path = BASE_DIR + '/jishu_stock/stockdata/stockcodelist_No_ST-1.csv'
-    count = 0
-    data = pd.read_csv(path, dtype={'code': str})
-
-
-    for index, row in data.iterrows():
-        # print row['ts_code']
-        stock_code = row['ts_code']
-        start_date='20210811'
-        end_date = '20210820'
-        df = ts.pro_bar(ts_code=stock_code, start_date=start_date, end_date=end_date)
-        # print df
-        if(len(df) ==8 ):
-            count= count+1
             time.sleep(0.1)
 
-            print count
-            close_20 = df.ix[0]['close']
-            close_11 = df.ix[7]['close']
+            # print count
+            close_start = df.ix[0]['close']
+            close_end = df.ix[len_df-1]['close']
 
-            if(close_20 > close_11):
+
+            if(close_end > close_start):
+                count = count + 1
                 # line = {'ts_code': stock_code}
-                path1 = BASE_DIR + '/jishu_stock/stockdata/qiangshi_stock.csv'
+                # path1 = BASE_DIR + '/jishu_stock/stockdata/qiangshi_stock.csv'
+                # print df
+                path1 = 'qiangshi_stock.csv'
                 writeLog_to_txt_path(stock_code,path1)
                 print stock_code
+    print count
 
 
 
--- jishu_stock/agetdata/RunAll.py
+++ jishu_stock/agetdata/RunAll.py
@@ -8,13 +8,11 @@ from jishu_stock.DaYou.DaYou import isAn_DaYou_model
 from jishu_stock.FanKeWeiZhu.FanKeWeiZhu import isAn_FanKeWeiZhu_model
 from jishu_stock.FanKeWeiZhu.FanKeWeiZhu_Plus import isAn_FanKeWeiZhu_Plus_model
 from jishu_stock.FeiLongZaiTian.FeiLongZaiTian import isAn_FeiLongZaiTian_model
-from jishu_stock.FeiLongZaiTian.FeiLongZaiTian2 import isAn_FeiLongZaiTian2_model
 from jishu_stock.FengHuiLuZhuan.FengHuiLuZhuan import isAn_FengHuiLuZhuan_model
 from jishu_stock.GeShanDaNiu.GeShanDaNiu import isAn_GeShanDaNiu_model
 from jishu_stock.GeShiBaFa.G8M2_RuanAll import G8M2_yijianyunxing
+from jishu_stock.JiaGeZhongShu.GetToday_JGZS import GetToday_JGZS
 from jishu_stock.JiaGeZhongShu.JGZS_RuanAll import JGZS_yijianyunxing
-from jishu_stock.JianLongZaiTian.JianLongZaiTian1 import isAn_JianLongZaiTian1_model
-from jishu_stock.JianLongZaiTian.JianLongZaiTian2 import isAn_JianLongZaiTian2_model
 from jishu_stock.JianLongZaiTian.JianLongZaiTian5 import isAn_JianLongZaiTian5_model
 from jishu_stock.JieJie.JieJie import isAn_JieJie_model
 from jishu_stock.JiuSiYiSheng.jiusiyisheng1 import isAn_jiusiyisheng1_model
@@ -23,13 +21,11 @@ from jishu_stock.KangLongyouHui.KangLongYouHuiGeGu import isAn_KangLongYouHui_Ge
 from jishu_stock.KangLongyouHui.KangLongYouHui_DaPan import get_all_KangLongYouHui_DaPan
 from jishu_stock.LiuAnHuaMing.LiuAnHuaMing2 import isAn_LiuAnHuaMing2_model
 from jishu_stock.LongZhan_YuYe.LongZhanYuYe import isAn_LongZhanYuYe_model
-from jishu_stock.LongZhan_YuYe.LongZhanYuYe2 import isAn_LongZhanYuYe2_model
 from jishu_stock.PiJiTaiLai.PiJiTaiLai import get_all_PiJiTaiLai
 from jishu_stock.QiBaoJunXian.QiBaoJunXian1 import isAn_QiBaoJunXian1_model
 from jishu_stock.QiBaoJunXian.QiBaoJunXian2 import isAn_QiBaoJunXian2_model
 from jishu_stock.QiBaoJunXian.QiBaoJunXian3 import isAn_QiBaoJunXian3_model
 from jishu_stock.QiBaoJunXian.QiBaoJunXian4 import isAn_QiBaoJunXian4_model
-from jishu_stock.SCS.SCS1 import isAn_SCS_1_model
 from jishu_stock.SiHuiFuRan.SiHuiFuRan import isAn_SiHuiFuRan_model
 from jishu_stock.Tool_jishu_stock import get_2stockcode, get_all_codes_from_tool, \
     csv_paixu_path1_zhuanyong
@@ -42,13 +38,17 @@ from jishu_stock.agetdata.getdata_2theard import get_all_codes
 from jishu_stock.agetdata.test_ziji_model.ZTB_XiaoYinXian_TiaoKong import isAn_ZTB_YinXian_TiaoKong_model
 from jishu_stock.bCaoMaoGui.NXingFanZhuan import isAn_NXingFanZhuan_model
 from jishu_stock.bCaoMaoGui.TaXingDi import isAn_TaXingDi_model
-from jishu_stock.bCaoMaoGui.YiYangChuanDuoJun import isAn_YiYangChuanDuoJun_model
 from jishu_stock.bCaoMaoGui.ZaoChenZhiXing import isAn_ZaoChenZhiXing_model
-from jishu_stock.bCaoMaoGui.ZaoChenZhiXing2 import isAn_ZaoChenZhiXing2_model
+from jishu_stock.bWuWei.DuiLiang1 import isAn_DuiLiang1_model
+from jishu_stock.bWuWei.FanBao1 import isAn_FanBao1_model
+from jishu_stock.bWuWei.Wu_LianYang import isAn_5LianYang_model
+from jishu_stock.bWuWei.XiaoYangJianCang import isAn_XiaoYangJianCang_model
+from jishu_stock.bWuWei.Yang4_1ZhangTB import isAn_4Yang1ZTB_model
 from jishu_stock.bWuWei.ZhuiJiYiZiBan import isAn_ZhuiJiYiZiBan_model
 from jishu_stock.cShenLongBaiWei.Shen1 import isAn_Shen1_model
 from jishu_stock.cShenLongBaiWei.ShenLongBaiWei0 import isAn_ShenLongBaiWei0_model
 from jishu_stock.cShenLongBaiWei.ShenLongBaiWei2 import get_all_ShenLongBaiWei2
+from jishu_stock.cShenLongBaiWei.ShenLongBaiWei2_Pro import isAn_ShenLongBaiWei2_Pro_model
 from jishu_stock.cShenLongBaiWei.ShenLongBaiWei3 import isAnShenLongBaiwei3_model
 from jishu_stock.cShenLongBaiWei.ShenLongBaiWei4 import get_all_ShenLongBaiWei4
 from jishu_stock.VXingFanZhuan.VXingFanzhuan_all import isAnV_model
@@ -57,6 +57,7 @@ from jishu_stock.LingBoWeiBu.LingBoWeiBu import get_all_LingBoWeiBu
 from jishu_stock.JiuSiYiSheng.JiuSiYiSheng2 import isAn_JiuSiYiSheng_2_model
 from jishu_stock.YiYiDaiLao.yiyidailaoAll.YiYiDaiLao0 import isAn_YiYiDaiLao_model
 from jishu_stock.YiJIanShuangDiao.YiJianShuangDiao import isAn_YiJianShuangDiao_model
+from jishu_stock.cShenLongBaiWei.Shen1_Pro import isAn_Shen1_Pro_model
 from jishu_stock.zYouQianJun.YouQianJun120_250 import get_all_120_250
 
 
@@ -105,11 +106,28 @@ def yijianyunxing():
     task4()
     csv_paixu_path1_zhuanyong()
 
+    #根据股票代码 获取 当天是不是价格中枢
+    GetToday_JGZS()
+
+
+
 """
 主要是 今天要看的模型数据
 """
 def task1(data6_1,stock_code):
 
+    #5连阳,开盘价依次提高
+    isAn_5LianYang_model(data6_1, stock_code)
+
+    # 4阳+ 1个涨停板
+    isAn_4Yang1ZTB_model(data6_1, stock_code)
+
+    #堆量 为了找小阳建仓
+    isAn_DuiLiang1_model(data6_1, stock_code)
+    #小阳建仓1
+    isAn_XiaoYangJianCang_model(data6_1, stock_code)
+    #反包 1 记住那一句话，该弱的不弱必强
+    isAn_FanBao1_model(data6_1, stock_code)
     #阴错阳差
     isAn_YinCuoYangCha_model(data6_1, stock_code)
     #李栋 追击一字板
@@ -127,8 +145,7 @@ def task1(data6_1,stock_code):
     #见龙在田5
     isAn_JianLongZaiTian5_model(data6_1, stock_code)
 
-    # 早晨之星2 十字星下影线比实体大 1.5 倍
-    # isAn_ZaoChenZhiXing2_model(data6_1, stock_code)
+
 
     #出水芙蓉 主力底部强势洗盘
     isAn_ChuShuiFuRong_model(data6_1, stock_code)
@@ -145,18 +162,12 @@ def task1(data6_1,stock_code):
     #以逸待劳
     isAn_YiYiDaiLao_model(data6_1, stock_code)
 
-    # 晓波 SCS1
-    # isAn_SCS_1_model(data6_1, stock_code)
+
     # 死灰复燃
     isAn_SiHuiFuRan_model(data6_1, stock_code)
     # 柳暗花明 底部强势上涨  , 每天标记 ,后期出现 等模型可以加仓
     isAn_LiuAnHuaMing2_model(data6_1, stock_code)
-    # 见龙在田1 判断 3个 K 线 , 一阳一阴一阳
-    # isAn_JianLongZaiTian1_model(data6_1, stock_code)
 
-
-    # 结界, 后期有账户再操作
-    isAn_JieJie_model(data6_1, stock_code)
     #起爆均线 1
     isAn_QiBaoJunXian1_model(data6_1, stock_code)
     #起爆均线 2
@@ -169,8 +180,7 @@ def task1(data6_1,stock_code):
     isAn_WuLiKanHua_model(data6_1, stock_code)
 
 
-    #看跌做涨 上涨结构 抄底 尾盘买入
-    isAn_KanDieZuoZhang_model(data6_1, stock_code)
+
     # 飞龙在天, 是萧先生 所有模型里边,挣钱最多, 最快的一个.  胜率最高的
     isAn_FeiLongZaiTian_model(data6_1, stock_code)
     #反客为主 上涨 一天强势洗盘 胜率 80%
@@ -185,10 +195,14 @@ def task1(data6_1,stock_code):
 
 
     #  神龙摆尾1 神1
-    isAn_Shen1_model(data6_1, stock_code)  #pro
+    isAn_Shen1_model(data6_1, stock_code)
+
+    #神1  6天数据模型
+    isAn_Shen1_Pro_model(data6_1, stock_code)  #pro  济民医疗
+    # 神2pro 改进版, 之前的有些拿不到
+    isAn_ShenLongBaiWei2_Pro_model(data6_1, stock_code)
     #  神龙摆尾3 神3
     isAnShenLongBaiwei3_model(data6_1, stock_code)
-
 
 
     #隔山打牛
@@ -197,10 +211,31 @@ def task1(data6_1,stock_code):
     # 熊市末期亢龙有悔 个股上的应用:
     isAn_KangLongYouHui_GeGu_model(data6_1, stock_code)
 
+
+'''
+目前精力不放在这里的
+'''
+def task4(data6_1,stock_code):
+
+    # 晓波 SCS1
+    # isAn_SCS_1_model(data6_1, stock_code)
+
+    # 见龙在田1 判断 3个 K 线 , 一阳一阴一阳
+    # isAn_JianLongZaiTian1_model(data6_1, stock_code)
+
+    # 结界, 后期有账户再操作
+    # isAn_JieJie_model(data6_1, stock_code)
+
+    #看跌做涨 上涨结构 抄底 尾盘买入
+    # isAn_KanDieZuoZhang_model(data6_1, stock_code)
+
+    # 早晨之星2 十字星下影线比实体大 1.5 倍
+    # isAn_ZaoChenZhiXing2_model(data6_1, stock_code)
+
+
     #一阳穿多均
     # isAn_YiYangChuanDuoJun_model(data6_1, stock_code)
-
-
+    print 'task4目前精力不放在这里的运行完了'
 '''
 明天要盯盘的任务
 '''
